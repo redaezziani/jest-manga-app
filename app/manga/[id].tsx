bd7088ada61b70733c12165161a693d516e04a8e
redaezzaini <redaezziani.dev@gmail.com>
2025-08-26 16:27:06 +0100

    feat: refactor MangaDetail component for improved layout and navigation

--- app/manga/[id].tsx
+++ app/manga/[id].tsx
@@ -1,6 +1,14 @@
-import { useLocalSearchParams } from "expo-router";
+import { Stack, useLocalSearchParams, useRouter } from "expo-router";
+import { ChevronLeft } from "lucide-react-native";
 import React, { useEffect, useState } from "react";
-import { Image, Platform, ScrollView, Text, View } from "react-native";
+import {
+  Image,
+  Platform,
+  ScrollView,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 interface Manga {
   id: string;
@@ -53,111 +61,144 @@ export default function MangaDetail() {
     );
   }
 
+  const router = useRouter();
+
   return (
-    <ScrollView className="flex-1 bg-white px-2">
-      {manga && (
-        <>
-          <Image
-            source={{ uri: manga.cover }}
-            style={{ width: 220, height: 350 }}
-            className="my-4 rounded-md border border-gray-300"
-          />
+    <>
+      {/* Hide Expo Router default header */}
+      <Stack.Screen options={{ headerShown: false }} />
+      <View className="flex-row  flex items-center justify-between h-28 px-4 py-3 bg-white border-b border-gray-200 shadow-sm">
+        <Text
+          style={{ fontFamily: "Arabic" }}
+          className="text-lg flex-row font-bold mt-2 text-gray-900"
+          numberOfLines={1}
+        >
+          تفاصيل المانجا {manga?.title}
+        </Text>
+        <TouchableOpacity onPress={() => router.back()} className="">
+          <ChevronLeft size={20} color="#4B5563" />
+        </TouchableOpacity>
+      </View>
+      <ScrollView className="flex-1 bg-white px-2">
+        {manga && (
+          <>
+            <Image
+              source={{ uri: manga.cover }}
+              style={{ width: 220, height: 350 }}
+              className="my-4 rounded-md border border-gray-300"
+            />
 
-          <View className="px-2 py-2">
-            <Text
-              style={{ fontFamily: "Arabic" }}
-              className="text-2xl font-bold text-gray-900 mb-1"
-            >
-              {manga.title}
-            </Text>
-
-            {manga.otherTitles.length > 0 && (
+            <View className="px-2 py-2">
               <Text
                 style={{ fontFamily: "Arabic" }}
-                className="text-gray-500 mb-2"
+                className="text-2xl font-bold text-gray-900 mb-1"
               >
-                {manga.otherTitles.join(" / ")}
-              </Text>
-            )}
-
-            <View className="mb-3">
-              <Text style={{ fontFamily: "Arabic" }} className="text-gray-700">
-                <Text style={{ fontFamily: "Arabic" }} className="font-bold">
-                  المؤلف:{" "}
-                </Text>
-                {manga.authors.join(", ")}
+                {manga.title}
               </Text>
 
-              <Text style={{ fontFamily: "Arabic" }} className="text-gray-700">
-                <Text style={{ fontFamily: "Arabic" }} className="font-bold">
-                  الرسام:{" "}
+              {manga.otherTitles.length > 0 && (
+                <Text
+                  style={{ fontFamily: "Arabic" }}
+                  className="text-gray-500 mb-2"
+                >
+                  {manga.otherTitles.join(" / ")}
                 </Text>
-                {manga.artists.join(", ")}
-              </Text>
+              )}
 
-              <Text style={{ fontFamily: "Arabic" }} className="text-gray-700">
-                <Text style={{ fontFamily: "Arabic" }} className="font-bold">
-                  النوع:{" "}
+              <View className="mb-3">
+                <Text
+                  style={{ fontFamily: "Arabic" }}
+                  className="text-gray-700"
+                >
+                  <Text style={{ fontFamily: "Arabic" }} className="font-bold">
+                    المؤلف:{" "}
+                  </Text>
+                  {manga.authors.join(", ")}
                 </Text>
-                {manga.type}
-              </Text>
 
-              <Text style={{ fontFamily: "Arabic" }} className="text-gray-700">
-                <Text style={{ fontFamily: "Arabic" }} className="font-bold">
-                  الحالة:{" "}
+                <Text
+                  style={{ fontFamily: "Arabic" }}
+                  className="text-gray-700"
+                >
+                  <Text style={{ fontFamily: "Arabic" }} className="font-bold">
+                    الرسام:{" "}
+                  </Text>
+                  {manga.artists.join(", ")}
                 </Text>
-                {manga.status}
-              </Text>
 
-              <Text style={{ fontFamily: "Arabic" }} className="text-gray-700">
-                <Text style={{ fontFamily: "Arabic" }} className="font-bold">
-                  المنصة:{" "}
+                <Text
+                  style={{ fontFamily: "Arabic" }}
+                  className="text-gray-700"
+                >
+                  <Text style={{ fontFamily: "Arabic" }} className="font-bold">
+                    النوع:{" "}
+                  </Text>
+                  {manga.type}
                 </Text>
-                {manga.platform}
+
+                <Text
+                  style={{ fontFamily: "Arabic" }}
+                  className="text-gray-700"
+                >
+                  <Text style={{ fontFamily: "Arabic" }} className="font-bold">
+                    الحالة:{" "}
+                  </Text>
+                  {manga.status}
+                </Text>
+
+                <Text
+                  style={{ fontFamily: "Arabic" }}
+                  className="text-gray-700"
+                >
+                  <Text style={{ fontFamily: "Arabic" }} className="font-bold">
+                    المنصة:{" "}
+                  </Text>
+                  {manga.platform}
+                </Text>
+              </View>
+
+              {/* Genres */}
+              {manga.genres.length > 0 && (
+                <View className="flex-row flex-wrap mb-4">
+                  {manga.genres.map((genre) => (
+                    <View
+                      key={genre}
+                      className="bg-gray-200 px-3 py-1 rounded-full mr-2 mb-2"
+                    >
+                      <Text
+                        style={{ fontFamily: "Arabic" }}
+                        className="text-sm text-gray-700"
+                      >
+                        {genre}
+                      </Text>
+                    </View>
+                  ))}
+                </View>
+              )}
+
+              {/* Description */}
+              <Text
+                style={{ fontFamily: "Arabic" }}
+                className="text-gray-800 leading-relaxed"
+              >
+                {manga.description}
               </Text>
             </View>
 
-            {/* Genres */}
-            {manga.genres.length > 0 && (
-              <View className="flex-row flex-wrap mb-4">
-                {manga.genres.map((genre) => (
-                  <View
-                    key={genre}
-                    className="bg-gray-200 px-3 py-1 rounded-full mr-2 mb-2"
-                  >
-                    <Text
-                      style={{ fontFamily: "Arabic" }}
-                      className="text-sm text-gray-700"
-                    >
-                      {genre}
-                    </Text>
-                  </View>
-                ))}
+            {/* Similar Manga */}
+            {similar.length > 0 && (
+              <View className="py-4 px-4">
+                <Text
+                  style={{ fontFamily: "Arabic" }}
+                  className="text-xl  text-gray-900 mb-2"
+                >
+                  مانجا مشابهة
+                </Text>
               </View>
             )}
-
-            {/* Description */}
-            <Text
-              style={{ fontFamily: "Arabic" }}
-              className="text-gray-800 leading-relaxed"
-            >
-              {manga.description}
-            </Text>
-          </View>
-
-          {/* Similar Manga */}
-          {similar.length > 0 && (
-            <View className="py-4 px-4">
-              <Text
-                style={{ fontFamily: "Arabic" }}
-                className="text-xl  text-gray-900 mb-2"
-              >
-                مانجا مشابهة
-              </Text>
-            </View>
-          )}
-        </>
-      )}
-    </ScrollView>
+          </>
+        )}
+      </ScrollView>
+    </>
   );
 }
