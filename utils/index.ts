cd5a7a817c50bcc8a2df7317c989e26a9c1e564e
redaezziani <klausdev2@gmail.com>
2025-09-09 21:52:33 +0100

    fix: Update API URL for Android Emulator to correct host address

--- utils/index.ts
+++ utils/index.ts
@@ -2,7 +2,7 @@ import { Platform } from "react-native";
 
 const API_URL =
   Platform.OS === "android"
-    ? "http://10.0.2.2:8082" // Android Emulator -> host machine
+    ? "http://192.168.1.126:8082" // Android Emulator -> Host machine
     : "http://localhost:8082"; // iOS simulator / web
 
 export { API_URL };
